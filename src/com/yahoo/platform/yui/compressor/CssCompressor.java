aimicheng/yuicompressor

commit caad3b2380520aa97ad8bb9c8e7453aec9ce8952

Fix CSS keyframes compression issue

src/com/yahoo/platform/yui/compressor/CssCompressor.java
@@ -96,6 +96,7 @@ public class CssCompressor {
         return sb.toString();
     }
 
+
     public void compress(Writer out, int linebreakpos)
             throws IOException {
 
@@ -320,9 +321,9 @@ public class CssCompressor {
 
         // Replace 0(px,em,%) with 0.
         css = css.replaceAll("(?i)(^|[^\\.0-9])(?:0?\\.)?0(?:px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz)", "$10");
-        css = css.replaceAll("(@(?:-.+?)?keyframe.*?)0\\{", "$10%{"); 
+        css = css.replaceAll("(@(?:-.+?)?keyframes.*?)0\\{", "$10%{");
         // Replace x.0(px,em,%) with x(px,em,%).
-        css = css.replaceAll("([0-9])\\.0(px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz| |;)", "$1$2");
+        css = css.replaceAll("(?i)(^|[^0-9])(?:0?\\.)?0(?:px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz)", "$10");
 
         // Replace 0 0 0 0; with 0.
         css = css.replaceAll(":0 0 0 0(;|})", ":0$1");

tests/keyframes.css
@@ -1,0 +1,16 @@
+@keyframes anim {
+    0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+@-webkit-keyframes anim {
+    0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+@-moz-keyframes anim {
+    0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+@-o-keyframes anim {
+    0% { opacity: 0; }
+  100% { opacity: 1; }
+}

tests/keyframes.css.min
@@ -1,0 +1,1 @@
+@keyframes anim{0%{opacity:0}100%{opacity:1}}@-webkit-keyframes anim{0%{opacity:0}100%{opacity:1}}@-moz-keyframes anim{0%{opacity:0}100%{opacity:1}}@-o-keyframes anim{0%{opacity:0}100%{opacity:1}}

commit cda1429bf26025b65826a28c7fe5240fca7b68ab

fix error encoding 0% introduced with caad3b2380520aa97ad8bb9c8e7453aec9ce8952

src/com/yahoo/platform/yui/compressor/CssCompressor.java
@@ -330,9 +330,9 @@ public class CssCompressor {
 
         // Replace 0(px,em,%) with 0.
         css = css.replaceAll("(?i)(^|[^\\.0-9])(?:0?\\.)?0(?:px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz)", "$10");
-        css = css.replaceAll("(@(?:-.+?)?keyframes.*?)0(\\{|,)", "$10%$2");
         // Replace x.0(px,em,%) with x(px,em,%).
-        css = css.replaceAll("(?i)(^|[^0-9])(?:0?\\.)?0(?:px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz)", "$10");
+        css = css.replaceAll("([0-9])\\.0(px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz| |;)", "$1$2");
+        css = css.replaceAll("(@(?:-.+?)?keyframes.*?)0(\\{|,)", "$10%$2");
 
         // Replace 0 0 0 0; with 0.
         css = css.replaceAll(":0 0 0 0(;|})", ":0$1");